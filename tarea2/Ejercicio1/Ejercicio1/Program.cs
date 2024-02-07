e6d198dbe26ea986987c2c0d9a5c8db307b49734
Kevin-Camb09 <156979129+Kevin-Camb09@users.noreply.github.com>
2024-02-07 10:03:23 -0600

    Add files via upload

---
+++ tarea2/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Ejercicio 1
+
+int[,] matriz = new int[3, 3];
+
+int result = 0;
+int val = 1;
+int sum = 0;
+Random rand = new Random();
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        matriz[i, j] = val++;
+        if (j >= i)
+        {
+            sum += matriz[i, j]; //acumulador
+
+        }
+        result = sum - 6;
+
+        Console.Write($"    {matriz[i, j]}");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("La suma es igual a: " + result);
---
+++ tarea2/Ejercicio2/Tarea2/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Ejercicio 2
+
+int[,] matriz = new int[3, 3];
+
+int result = 0;
+int val = 1;
+int sum = 0;
+Random rand = new Random();
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        matriz[i, j] = val++;
+        if (j > i)
+        {
+            sum += matriz[i, j]; //acumulador
+
+        }
+        result=sum+4;
+
+        Console.Write($"    {matriz[i, j]}");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("La suma es igual a: "+ result);
+
+
+
+
+
